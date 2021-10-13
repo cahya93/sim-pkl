d851e07f7a2cb61515664ba9f4a75e3e0a96c5ea
cahya93 <ekocahyanto007@gmail.com>
2021-10-13 08:54:48 +0700

    update menu

--- sim-muhka/src/component/menu/menu.js
+++ sim-muhka/src/component/menu/menu.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import "./menu.css";
 class menu extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    const { children, action, styles } = this.props;
+    const { children, action } = this.props;
     return (
-      <div className={styles} onClick={action}>
+      <div className="menu" onClick={action}>
         {children}
       </div>
     );

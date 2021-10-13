0cca9add07696894abaaa0a0cdaf06152f6ff411
cahya93 <ekocahyanto007@gmail.com>
2021-10-13 08:49:09 +0700

    update app.js

--- sim-muhka/src/App.js
+++ sim-muhka/src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import Home from "./template/home/";
-import Admin from "./template/admin";
 import { connect } from "react-redux";
-// import Api from "./service/api/api";
 import React, { Component } from "react";
-// const passwordHash = require("password-hash");
+import Home from "./page/home/home";
+import Dashboard from "./page/dashboard/dashboard";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,63 +10,14 @@ class App extends Component {
       isLogin: "",
     };
   }
-  // handleChange = (event) => {
-  //   this.setState({ [event.target.name]: event.target.value });
-  // };
-  // getData = () => {
-  //   Api.getUser().then((data) => {
-  //     console.log(`data`, data.data);
-  //     this.setState({
-  //       listUser: data.data,
-  //     });
-  //   });
-  // };
-  // renderData = () => {
-  //   const { listUser } = this.state;
-  //   return (
-  //     <table border="1">
-  //       <thead>
-  //         <th>
-  //           <tr>No</tr>
-  //         </th>
-  //         <th>
-  //           <tr>Nama</tr>
-  //         </th>
-  //         <th>
-  //           <tr>Email</tr>
-  //         </th>
-  //         <th>
-  //           <tr>Password</tr>
-  //         </th>
-  //       </thead>
-  //       <tbody>
-  //         {listUser.map((data, idx) => (
-  //           <tr key={idx}>
-  //             <td>{idx + 1}</td>
-  //             <td>{data.name}</td>
-  //             <td>{data.email}</td>
-  //             <td>{data.password}</td>
-  //           </tr>
-  //         ))}
-  //       </tbody>
-  //     </table>
-  //   );
-  // };
-  // componentDidMount() {
-  //   this.getData();
-  // }
-  // handleSubmit = () => {
-  //   const { password } = this.state;
-  //   var pass = "sha1$822cffde$1$5473f2fe42997251482825e3a713108a907a9fc2"; //12345
-  //   var hashedpassword = passwordHash.verify(password, pass);
-  //   console.log(`hashedpassword`, hashedpassword);
-  // };
-  renderPage = () => {
-    // const { isLogin } = this.state;
-    if (!this.props.isLogedIn) return <Home />;
-    if (this.props.isLogedIn) return <Admin />;
-  };
 
+  renderPage = () => {
+    if (!this.props.isLogedIn) return <Home />;
+    if (this.props.isLogedIn) return <Dashboard />;
+  };
+  componentDidMount = () => {
+    this.renderPage();
+  };
   render() {
     console.log(`isLogin`, this.props.isLogedIn);
     return <div>{this.renderPage()}</div>;

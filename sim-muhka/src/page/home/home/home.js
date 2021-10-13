d799e7c6a09cb78183aaef83d0f53afc9ebf9f7b
cahya93 <ekocahyanto007@gmail.com>
2021-10-13 08:59:20 +0700

    update home page

--- sim-muhka/src/page/home/home/home.js
+++ sim-muhka/src/page/home/home/home.js
@@ -1,16 +1,19 @@
-import "./home.css";
 import { Title } from "../../../component";
+import "./home.css";
 import React, { Component } from "react";
 class home extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   render() {
     return (
-      <div className="title">
-        <Title>Selamat datang di aplikasi E-PKL</Title>
-        <Title>SMK Muhammadiyah Karangmojo</Title>
+      <div>
+        <div className="title">
+          <Title>Selamat datang di aplikasi E-PKL</Title>
+          <Title>SMK Muhammadiyah Karangmojo</Title>
+        </div>
       </div>
     );
   }
--- sim-muhka/src/page/home/juknis/juknis.js
+++ sim-muhka/src/page/home/juknis/juknis.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+
 class juknis extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div>ini page juknis</div>;
+    return (
+      <div>
+        <div>ini page juknis</div>
+      </div>
+    );
   }
 }
 

66d960c04705f090feafc4eb4e891d71abc82bd7
cahya93 <ekocahyanto007@gmail.com>
2021-10-15 18:11:37 +0700

    update struktur tabel

--- backend-java-sim-pkl/src/main/java/com/muhka/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.muhka.repository;
-
-import com.muhka.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
